d941cb1254d04a2128af6665860db70123cf1c5c
Asmaul Mallick <145951970+Asmaul360@users.noreply.github.com>
2026-03-14 19:29:32 +0530

    added comment in the adding element in an array

--- arrays/addingElement.java
+++ arrays/addingElement.java
@@ -1,3 +1,31 @@
+/*
+Question:
+Write a Java program to insert an element into an array at a given position.
+The program should:
+1. Take the size of the array as input.
+2. Take the array elements from the user.
+3. Take an element to be inserted.
+4. Take the position where the element should be inserted.
+5. Create a new array of size (n + 1).
+6. Insert the element at the given position and shift the remaining elements to the right.
+7. Print the new array.
+
+Example:
+
+Input:
+Size of array = 4
+Array elements = 10 20 30 40
+Element to insert = 25
+Position = 2
+
+Output:
+New array elements are:
+10
+20
+25
+30
+40
+*/
 package arrays;
 import java.util.Scanner;
 public class addingElement {
