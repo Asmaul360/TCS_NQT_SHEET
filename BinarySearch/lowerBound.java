bfdea1e58b6dbc4f163602d81905f6edfaf122d6
Asmaul Mallick <145951970+Asmaul360@users.noreply.github.com>
2026-04-15 00:24:08 +0530

    15/4/2026

--- BinarySearch/lowerBound.java
+++ BinarySearch/lowerBound.java
@@ -2,29 +2,27 @@ package BinarySearch;
 
 public class lowerBound {
     public static void main(String[] args) {
-        int arr[]={3,5,8,15,19};
-        int target=9;
-        int ans=search(arr,target);
+        int arr[] = {1,2,2,3};
+        int target = 2;
+        int ans = search(arr, target);
         System.out.println(ans);
     }    
-    static int search(int arr[],int target){
-        if(arr.length==0){
-            return 1;
-        }
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]<target){
-                start=mid+1;
-            }
-            else if (arr[mid]>target){
-                end=mid-1;
-            }
-            else{
-                return mid;
+
+    static int search(int arr[], int target){
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = arr.length; // default if not found
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] >= target){
+                ans = mid;        // store possible answer
+                end = mid - 1;    // go left
+            } else {
+                start = mid + 1;  // go right
             }
         }
-        return arr[start] ;
+        return ans;
     }
 }
